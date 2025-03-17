6405da847030c2433434635f55abf4c786213b3d
kiran1926 <kiranroge19@gmail.com>
2025-03-16 20:30:09 -0500

    wip: created project and task models

---
+++ models/task.js
@@ -0,0 +1,40 @@
+
+const mongoose = require("mongoose");
+
+const taskSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    status: {
+        type: String,
+        enum: ["Not Started", "In Progress", "Completed"],
+        default: "Not Started",
+    },
+    priority: {
+        type: String,
+        enum: ["Low", "Medium", "High"],
+        default: "Low",
+    },
+    dueDate: {
+        type: Date,
+    },
+    // attachments: {
+    //     type: String,  // stores file path
+    // },
+    projectId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Project",
+        required: true,
+    },
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+}, {timestamp : true});
+
+module.exports = mongoose.model("Task", taskSchema);
