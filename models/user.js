43ea50920c6af01901abdd6bc11db255ac0d4653
kiran1926 <kiranroge19@gmail.com>
2025-03-21 02:39:38 -0500

    wip : code formats

--- models/user.js
+++ models/user.js
@@ -1,25 +1,27 @@
-
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        enum: ["admin", "owner", "member"],
-        default : "member",
+      type: String,
+      enum: ["admin", "owner", "member"],
+      default: "member",
     },
-}, {timestamp : true});
+  },
+  { timestamp: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
